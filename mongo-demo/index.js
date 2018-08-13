ebcadb0a923e5c18979f79f7eac1530d9ef95f85
Colin Lancaster <lancaster.colin@gmail.com>
2018-08-12 19:38:58 -0700

    started new section

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,3 +1,43 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/playground');
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
+  // Once we have a schema...
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+// ... We need to compile it into model.
+const Course = mongoose.model('Course', courseSchema); // Remember this is a class
+
+async function createCourse() {
+  try {
+    const course = new Course({
+      name: 'Angular Course',
+      author: 'Colin',
+      tags: ['angular', 'frontend'],
+      isPublished: true
+    }); // Remember this is an object
+
+    const result = await course.save();
+    console.log(result);
+  } catch (error) {
+      console.log('Error', error)
+  }
+}
+
+async function getCourses() {
+  const courses = await Course
+    .find({ author: 'Colin', isPublished: true }) // This is a 'filter'
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1});
+  console.log(courses);
+}
+
+getCourses();
