afd639067e3f167171ab3672093b72e193c0b0cf
Colin Lancaster <lancaster.colin@gmail.com>
2018-10-26 13:07:39 -0700

    big commit from master

--- 2nd-Try/vidly/index.js
+++ 2nd-Try/vidly/index.js
@@ -1,24 +1,26 @@
 // This entire file is known as the Request Pipeline
 
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 const home = require('./routes/home');
 const app = express();
 
 
 mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err = console.error('Could not connect to MongoDB'));
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded()); //key=value&key=value.
+// app.use(express.urlencoded()); //key=value&key=value.
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 app.use('/', home);
 
 // PORT
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ 2nd-Try/vidly/models/genre.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+});
+// You can create the schema from within the model
+// Or you can store the model in a const
+const Genre = mongoose.model('Genre', genreSchema);
+
+function validateGenre(genre) {
+  // Validate
+  // If invalid, return 400 - Bad Req
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+exports.genreSchema = genreSchema;
+exports.Genre = Genre;
+exports.validate = validateGenre;
