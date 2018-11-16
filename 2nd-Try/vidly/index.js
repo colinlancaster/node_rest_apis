896114d62cf3a1fb32189f0d028669177c4e44b3
Colin Lancaster <lancaster.colin@gmail.com>
2018-11-15 22:54:02 -0800

    finished up integration testing section

--- 2nd-Try/vidly/index.js
+++ 2nd-Try/vidly/index.js
@@ -10,6 +10,7 @@ require('./startup/config')();
 require('./startup/validation')();
 
 const port = process.env.PORT || 3000;
+
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
